bdede1b030c2dd4fcd8c87a6cc4b8e92bd8d597d
Nillerr <800751+Nillerr@users.noreply.github.com>
2017-08-27 01:28:06 +0200

    Adds unit tests for Json serialization

---
+++ Amplified.ValueObjects.Tests/Newtonsoft.Json/ValueObjectJsonConverter.cs
@@ -0,0 +1,50 @@
+﻿using Amplified.ValueObjects.Newtonsoft.Json;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Amplified.ValueObjects.Tests.Newtonsoft.Json
+{
+    public sealed class JsonConverter
+    {
+        private readonly JsonSerializerSettings _settings = new JsonSerializerSettings()
+        {
+            Converters = {new ValueObjectJsonConverter()}
+        };
+        
+        [Fact]
+        public void ConvertIntValue()
+        {
+            const int value = 59;
+            var source = new IntValueObject(value);
+            var json = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal("59", json);
+        }
+        
+        [Fact]
+        public void ConvertIntValueAsProperty()
+        {
+            const int value = 59231;
+            var source = new IntValueObject(value);
+            var json = JsonConvert.SerializeObject(new { Prop = source }, _settings);
+            Assert.Equal(@"{""Prop"":59231}", json);
+        }
+        
+        [Fact]
+        public void ConvertStringValue()
+        {
+            const string value = "fooBar";
+            var source = new StringValueObject(value);
+            var json = JsonConvert.SerializeObject(source, _settings);
+            Assert.Equal(@"""fooBar""", json);
+        }
+        
+        [Fact]
+        public void ConvertStringValueAsProperty()
+        {
+            const string value = "FizzBuzz";
+            var source = new StringValueObject(value);
+            var json = JsonConvert.SerializeObject(new { Prop = source }, _settings);
+            Assert.Equal(@"{""Prop"":""FizzBuzz""}", json);
+        }
+    }
+}
